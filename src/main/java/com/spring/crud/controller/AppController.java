36212c2241abe52ff3deb9585e66de0d1f466c99
coffeinna <agata.kawecka@op.pl>
2022-05-20 08:54:39 +0200

    refactor

--- src/main/java/com/spring/crud/controller/AppController.java
+++ src/main/java/com/spring/crud/controller/AppController.java
@@ -1,12 +1,13 @@
-package com.spring.crud;
+package com.spring.crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class AppController {
 
-	@GetMapping("/")
-	public String showHomePage() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
 }
